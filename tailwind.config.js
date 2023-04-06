b91c07e01907f951640587ba68ed5d22e3dd6349
banuka <banukaramanayaka76@gmail.com>
2023-04-06 14:49:03 +0530

    gdfggfdsg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['index.html','index.js'],
+  content: ["index.html","test.html" ,"index.js"],
   theme: {
     extend: {
-      fontFamily:{
-        sans:['Be Vietnam Pro']
+      fontFamily: {
+        sans: ["Be Vietnam Pro"],
       },
-      colors:{
-        primary:'#ff6363',
-        secondary:{
-          100:'',
-          200:'',
-        }
-      }
+      // fontWeight:{
+      //   regular:400,
+      //   bold:900
+      // },
+      colors: {
+        BrightRed: "hsl(12, 88%, 59%)",
+        DarkBlue: "hsl(228, 39%, 23%)",
+        DarkGrayishBlue: "hsl(227, 12%, 61%)",
+        VeryDarkBlue: "hsl(233, 12%, 13%)",
+        VeryPaleRed: "hsl(13, 100%, 96%)",
+        VaryLightGray: "hsl(0, 0%, 98%)",
+      },
     },
   },
   plugins: [],
-}
-
+};
